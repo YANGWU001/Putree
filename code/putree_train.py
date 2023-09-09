96a71339625d807c41c002b50890550687a49745
Yang Wu <ywu7@gputee.luddy.indiana.edu>
2023-09-08 22:08:07 -0400

    putree_update

--- code/putree_train.py
+++ code/putree_train.py
@@ -53,7 +53,7 @@ def main(args):
     test = pd.read_csv(test_file)
     train = pd.read_csv(train_file)
     validation = test.copy() # Set the validation set to mirror the test set for monitoring the training process.
-
+    save_path = args.dataset + "_putree.png"
     putree = PUTreeClassifier(
         max_depth=args.max_depth,
         min_node_size=args.min_node_size,
@@ -65,7 +65,7 @@ def main(args):
         local_bet=args.local_bet,
         aug=args.aug,
         fus=args.fus,
-        save_path=args.save_path  
+        save_path=save_path 
     )
 
     putree.fit(train, validation)
@@ -90,7 +90,6 @@ if __name__ == "__main__":
     parser.add_argument("--local_bet", type=float, default=0.9, help="Local bet value")
     parser.add_argument("--aug", action="store_true", help="Enable data augmentation")
     parser.add_argument("--fus", action="store_true", help="Enable fusion")
-    parser.add_argument("--save_path", type=str, default="putree.png", help="Path to save the tree image")
     parser.add_argument("--dataset", type=str, default="unsw", help="Dataset choice (unsw, nsl_kdd, diabetes)")
     parser.add_argument("--gpu", action="store_true", help="Use GPU if available")
     args = parser.parse_args()
